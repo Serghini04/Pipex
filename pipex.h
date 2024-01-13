29b62c62ee19cb49c9483ddbea8f6fc703267c22
Mehdi Serghini <meserghi@e3r6p5.1337.ma>
2024-01-13 13:56:28 +0100

    u

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: meserghi <meserghi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 19:25:53 by meserghi          #+#    #+#             */
-/*   Updated: 2024/01/13 12:40:56 by meserghi         ###   ########.fr       */
+/*   Updated: 2024/01/13 13:56:09 by meserghi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ char	*ft_strjoin(char *s1, char *s2);
 int		ft_strlen(char *s);
 int		parsing_arg(char **av, char **path);
 char	*checker_cmd(char *str, char **path);
+void	free_arr(char **res);
 
 #endif
